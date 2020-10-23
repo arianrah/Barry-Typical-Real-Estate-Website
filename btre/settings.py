f2790133696b03cfa9b67731ab88af51d5d8aca7
arianrah <arianrahimi@icloud.com>
2020-10-23 01:27:54 -0700

    Brought in generic icons (loading) and lightbox images.

--- btre/settings.py
+++ btre/settings.py
@@ -1,4 +1,5 @@
 import os
+import mimetypes
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -108,7 +109,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIR = [
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
-STATIC_URL = '/static/'
+
+mimetypes.add_type("text/css", ".css", True)

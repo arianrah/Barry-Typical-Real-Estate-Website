51371b6d1227f1feed208d20a8e9cdf1d473d788
arianrah <arianrahimi@icloud.com>
2020-11-10 17:21:01 -0800

    Form handling and FE filtering completed.

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from listings.choices import price_choices, bedroom_choices, province_choices
 
 from .models import Listing
 
@@ -26,4 +27,45 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/searcb.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(
+                description__icontains=keywords)
+
+    # City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    # Province
+    if 'province' in request.GET:
+        province = request.GET['province']
+        if province:
+            queryset_list = queryset_list.filter(province__iexact=province)
+
+    # Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+
+    # Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+
+    context = {
+        'province_choices': province_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'listings': queryset_list,
+        'values': request.GET
+    }
+
+    return render(request, 'listings/search.html', context)
